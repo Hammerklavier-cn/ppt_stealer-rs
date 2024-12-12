aa8e77952cbcba225aeae45a9b91b6f984ddbbcb
hammerklavier <hammerklavier@noreply.gitcode.com>
2024-12-12 20:59:57 +0800

    feat: 添加命令行参数解析功能
    - 引入 clap 库用于解析命令行参数
    - 定义 Cli 结构体，包含 FTP 服务器 IP、端口、用户名和密码等参数
    - 添加参数分组和必选性约束，提高用户体验
    - 为项目增加了基本的命令行接口，为后续功能开发做准备

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use clap::{Parser, ArgGroup};
+
+#[derive(Parser)]
+#[command(name = "ppt_stealer-rs", version = "0.1")]
+#[command(about = "A tool to steal PowerPoint files from desktop", long_about = None)]
+#[command(group(
+    ArgGroup::new("auth")
+        .args(&["password", "key_auth"])
+        .required(true)
+        .multiple(false)
+))]
+struct Cli {
+    #[arg(short = 'i', long)]
+    ftp_ip: String,
+
+    #[arg(short = 'p', long)]
+    ftp_port: i64,
+
+    #[arg(short = 'u', long)]
+    username: String,
+
+    #[arg(short = 'P', long, group = "auth")]
+    password: String,
+
+    #[arg(long, default_value_t = false, group = "auth")]
+    key_auth: bool,
+}
+
+
 fn main() {
+    
+    // Parse command line arguments
+    let args = Cli::parse();
+
+    // check arguments
+    // check if ftp_ip and ftp_port are valid
+    
+
     println!("Hello, world!");
 }
