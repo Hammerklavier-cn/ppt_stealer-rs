111c457354a2fa4ae8ba947d5ed6b1a0cb41dc9b
hammerklavier <hammerklavier@noreply.gitcode.com>
2024-12-13 18:04:42 +0800

    feat: 添加日志功能并优化命令行参数处理
    - 引入 log 和 env_logger 依赖，用于日志记录
    - 添加 GUI 参数支持
    - 优化命令行参数解析，支持可选密码参数
    - 初始化日志记录并打印程序信息

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use clap::{Parser, ArgGroup};
+use log;
+use env_logger;
 
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 #[command(name = "ppt_stealer-rs", version = "0.1")]
 #[command(about = "A tool to steal PowerPoint files from desktop", long_about = None)]
 #[command(group(
@@ -20,21 +22,29 @@ struct Cli {
     username: String,
 
     #[arg(short = 'P', long, group = "auth")]
-    password: String,
+    password: Option<String>,
 
     #[arg(long, default_value_t = false, group = "auth")]
     key_auth: bool,
+
+    #[arg(long, default_value_t = false)]
+    gui: bool,
 }
 
 
 fn main() {
+
+    std::env::set_var("RUST_LOG", "debug");
+    env_logger::init();
     
     // Parse command line arguments
     let args = Cli::parse();
 
-    // check arguments
-    // check if ftp_ip and ftp_port are valid
-    
+    // Set up logging
+    // print name and version
+    log::info!("ppt_stealer-rs v0.1");
+    // print args
+    log::info!("Args: {:?}", args);
 
     println!("Hello, world!");
 }
